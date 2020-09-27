fa716d27a467f2dbf7d6383bd4cbd8e6abb135a2
Jon <jongilmore1@gmail.com>
2020-09-26 23:09:12 -0500

    feat: add garage camera automated restart via unifi poe switch toggle. fix lovelace temperature displays

--- scripts/unifi.sh
+++ scripts/unifi.sh
@@ -8,19 +8,33 @@ portfwd() {
   ${curl_cmd} -d "{\"username\":\"$1\", \"password\":\"$2\"}" https://${3}:8443/api/login
 
   # enable/disable firewall rule
-  ${curl_cmd} -k -X PUT https://${3}:8443/api/s/default/rest/portforward/${10} -H "Content-Type: application/json" -d @- <<EOF
-{
-  "name":"$4",
-  "enabled":$5,
-  "src":"any",
-  "dst_port":"$6",
-  "fwd":"$7",
-  "fwd_port":"$8",
-  "proto":"$9",
-  "log":false,
-  "_id":"$10",
-  "site_id":"$11"
+  ${curl_cmd} -k -X PUT https://${3}:8443/api/s/default/rest/portforward/${10} -H "Content-Type: application/json" -d @- <<-EOF
+  {
+    "name":"$4",
+    "enabled":$5,
+    "src":"any",
+    "dst_port":"$6",
+    "fwd":"$7",
+    "fwd_port":"$8",
+    "proto":"$9",
+    "log":false,
+    "_id":"$10",
+    "site_id":"$11"
+  }
+EOF
 }
+
+powercycleport() {
+  # authenticate against unifi controller
+  ${curl_cmd} -d "{\"username\":\"$1\", \"password\":\"$2\"}" https://${3}:8443/api/login
+
+  # cycle unifi port
+  ${curl_cmd} -k -X POST https://${3}:8443/api/s/default/cmd/devmgr -H "Content-Type: application/json" -d @- <<-EOF
+    {
+        "mac":"$4",
+        "port_idx":$5,
+        "cmd":"$6"
+    }
 EOF
 }
 
