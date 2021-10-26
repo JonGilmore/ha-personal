e332764cede3a6469e80481d90ea401b50714a73
Jon <jon@gilmore.cloud>
2021-10-25 21:54:08 -0500

    chore(nginx): prefer nginx over nabucasa allowlist cloudflare public IPs explicitly (unifi port foward automation to handle this big list) - enable/disable port fwd rules add `src` as an option to enabling/disable port fwd rules enable nginx addon - set trusted proxy rm nabucasa cloud disconect/reconnect

--- scripts/unifi.sh
+++ scripts/unifi.sh
@@ -14,16 +14,16 @@ portfwd() {
   # enable/disable firewall rule
   ${curl_cmd} -k -X PUT https://${3}/proxy/network/api/s/default/rest/portforward/${10} -H "Content-Type: application/json" -H "X-CSRF-Token: ${csrf}" -d @- <<-EOF
   {
-    "name":"$4",
-    "enabled":$5,
-    "src":"any",
-    "dst_port":"$6",
-    "fwd":"$7",
-    "fwd_port":"$8",
-    "proto":"$9",
+    "name":"${4}",
+    "enabled":${5},
+    "src":"${12}",
+    "dst_port":"${6}",
+    "fwd":"${7}",
+    "fwd_port":"${8}",
+    "proto":"${9}",
     "log":false,
-    "_id":"$10",
-    "site_id":"$11",
+    "_id":"${10}",
+    "site_id":"${11}",
     "pfwd_interface":"wan",
     "destination_ip":"any"
   }
